7c746b8315f091cdfe6fa79dd221301aeff0fead
Ladomitory <radomirholmogorov@yandex.ru>
2021-02-22 08:08:51 +0700

    начало создания тестера

---
+++ TesterLibraryBenarytree/main.cpp
@@ -0,0 +1,14 @@
+//tester.cpp
+
+#include <iostream>
+#include <fstream>
+#include "binarytree.h"
+
+using namespace std;
+using namespace binarytree;
+
+int main()
+{
+	cout << 1 << endl;
+	return 0;
+}
